84de7b787b8fd2ad1f109d23b758313434fce2d8
mustafaacan <mustafaa.can.3535@gmail.com>
2021-03-03 23:13:04 +0300

    My test C file

---
+++ test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int main(int argc, char const *argv[])
+{
+    for (size_t i = 0; i < 5; i++)
+    {
+        printf("Mstafa");
+    }
+    
+    return 0;
+}
